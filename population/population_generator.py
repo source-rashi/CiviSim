f927ae58f742a6d7d04c104ddf8d502c9c6372d0
CIVISIM Developer <developer@civisim.local>
2026-03-17 12:05:06 +0530

    feat: add synthetic population generator

--- population/population_generator.py
+++ population/population_generator.py
@@ -1,1 +1,63 @@
-# Population Generator
+import random
+from population.citizen import Citizen
+
+
+def generate_population(size):
+
+    population = []
+
+    for i in range(size):
+
+        citizen = Citizen(
+
+            cid=i,
+
+            age=random.randint(18, 70),
+
+            gender=random.choice(
+                ["male", "female"]
+            ),
+
+            income=random.randint(
+                10000,
+                200000
+            ),
+
+            occupation=random.choice([
+                "student",
+                "worker",
+                "business",
+                "government",
+                "unemployed"
+            ]),
+
+            caste=random.choice([
+                "general",
+                "obc",
+                "sc",
+                "st"
+            ]),
+
+            education=random.choice([
+                "school",
+                "college",
+                "phd"
+            ]),
+
+            location=random.choice([
+                "urban",
+                "semi-urban",
+                "rural"
+            ]),
+
+            traits={
+                "risk_tolerance": random.random(),
+                "openness": random.random(),
+                "political_leaning": random.random()
+            }
+
+        )
+
+        population.append(citizen)
+
+    return population
