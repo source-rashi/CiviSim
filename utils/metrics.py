47cc9975aae76c8e35f05ff60fc3d7d4aca0714c
CIVISIM Developer <developer@civisim.local>
2026-03-17 12:05:50 +0530

    feat: add population analytics utilities

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,1 +1,24 @@
-# Metrics
+from collections import Counter
+
+
+def occupation_distribution(population):
+
+    occupations = [c.occupation for c in population]
+    return dict(Counter(occupations))
+
+
+def caste_distribution(population):
+
+    castes = [c.caste for c in population]
+    return dict(Counter(castes))
+
+
+def income_list(population):
+
+    return [c.income for c in population]
+
+
+def average_income(population):
+
+    incomes = income_list(population)
+    return sum(incomes) / len(incomes)
